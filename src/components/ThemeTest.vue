4306106529cf553f12fa7a2e1026b4fb8a3c70f8
weifeng.zhang <34675974+Wulakaka@users.noreply.github.com>
2020-08-20 23:00:27 +0800

    add

---
+++ src/components/ThemeTest.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="dark-gray">gray</div>
+    <div class="dark-blue">blue</div>
+    <button class="button">button</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang='scss' scoped>
+</style>
